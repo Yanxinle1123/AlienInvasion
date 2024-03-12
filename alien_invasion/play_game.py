578048825d2865bc6189dcbb2447831c141cdce8
yanxinle <1020121123@qq.com>
2024-03-12 17:18:34 +0800

    设置窗口的颜色

--- alien_invasion/play_game.py
+++ alien_invasion/play_game.py
@@ -9,9 +9,13 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("外星人入侵")
+
+        # 设置背景色
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -21,8 +25,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # 每次循环时都重绘屏幕
+            self.screen.fill(self.bg_color)
+
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
