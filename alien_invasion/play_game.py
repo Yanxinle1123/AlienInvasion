7157562c2aece5e1f09c0202c668bbaf66c29a18
yanxinle <1020121123@qq.com>
2024-03-14 17:18:36 +0800

    重构代码

--- alien_invasion/play_game.py
+++ alien_invasion/play_game.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from alien_invasion.settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -17,20 +18,27 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("外星人入侵")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像, 并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -10,7 +10,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('/Users/lele/PycharmProjects/AlienInvasion/alien_invasion/images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # 每艘新飞船都放在屏幕底部的中央
