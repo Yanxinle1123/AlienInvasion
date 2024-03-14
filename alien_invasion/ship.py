7af145a7acfa32fd201821127cfc4bb9242ce657
yanxinle <1020121123@qq.com>
2024-03-14 20:07:37 +0800

    更新代码, 让飞船可以左右移动

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -16,12 +17,20 @@ class Ship:
         # 每艘新飞船都放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 在飞船的属性x中存储一个浮点数
+        self.x = float(self.rect.x)
+
         # 移动飞船
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
