ff2ccd6571a9dee55677e621b23b661e654dd4a4
yanxinle <1020121123@qq.com>
2024-03-14 20:19:18 +0800

    更新代码, 让飞船碰到边界不会移动

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -25,10 +25,12 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.x += self.settings.ship_speed
-        if self.moving_left:
-            self.x -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            if self.moving_right:
+                self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            if self.moving_left:
+                self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
 
