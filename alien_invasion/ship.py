233c77ec37745c89f57b50692e8588a41dc4187f
yanxinle <1020121123@qq.com>
2024-03-15 12:51:16 +0800

    重构代码

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -26,11 +26,9 @@ class Ship:
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            if self.moving_right:
-                self.x += self.settings.ship_speed
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            if self.moving_left:
-                self.x -= self.settings.ship_speed
+            self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
 
