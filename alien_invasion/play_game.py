eb303c155ec0a2001b7ad4c2512a819d3bf1c50c
yanxinle <1020121123@qq.com>
2024-03-15 13:41:33 +0800

    更新代码, 让飞机可以发射子弹

--- alien_invasion/play_game.py
+++ alien_invasion/play_game.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from alien_invasion.bullet import Bullet
 from alien_invasion.settings import Settings
 from ship import Ship
 
@@ -19,12 +20,14 @@ class AlienInvasion:
         pygame.display.set_caption("外星人入侵")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
             self._update_screen()
+            self.bullets.update()
             self.ship.update()
             self.clock.tick(60)
 
@@ -46,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """响应释放"""
@@ -54,9 +59,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """创建一颗子弹, 并将其假如编组bullets"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """更新屏幕上的图像, 并切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
