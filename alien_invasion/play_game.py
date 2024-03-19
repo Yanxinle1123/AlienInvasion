a072823bc901a865f20b1fa4ca2ae6ba406ba055
yanxinle <1020121123@qq.com>
2024-03-19 13:21:44 +0800

    更新代码, 限制子弹的数量

--- alien_invasion/play_game.py
+++ alien_invasion/play_game.py
@@ -27,16 +27,21 @@ class AlienInvasion:
         """开始游戏的主循环"""
         while True:
             self._check_events()
-            self._update_screen()
-            self.bullets.update()
-
-            # 删除消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
             self.ship.update()
+            self._update_bullets()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹"""
+
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         """响应按键和鼠标事件"""
@@ -68,8 +73,10 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """创建一颗子弹, 并将其假如编组bullets"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """更新屏幕上的图像, 并切换到新屏幕"""
