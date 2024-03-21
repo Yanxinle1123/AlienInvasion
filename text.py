1c0aa85216f7fdf65dde2579d1b43482c1fa4f80
yanxinle <1020121123@qq.com>
2024-03-21 12:59:10 +0800

    正在修复错误

--- text.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-print("屏幕宽度:", screen_width)
-print("屏幕高度:", screen_height)
